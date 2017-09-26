ff0a9a61e664f458d839d55407230da0c828bde4
J.D. Cain <jd.cain.jr@gmail.com>
2017-09-25 20:57:54 -0400

    init

---
+++ FG5eXmlToPdf/FG5eXmlToPdf.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FG5eXmlToPdf.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FG5eXmlToPdf.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb74cfc3-7bba-42ad-924f-01191dec4b05")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FG5eXmlToPdf/FG5eXmlToPdf/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FG5eXmlToPdf
+{
+    public class Class1
+    {
+    }
+}
