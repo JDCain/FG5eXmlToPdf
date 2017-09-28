ad5bc24513c17cc68c9887d75851d514aa7ad3d8
J.D. Cain <jd.cain.jr@gmail.com>
2017-09-28 09:07:05 -0400

    added console app template

---
+++ FG5eXmlToPdf.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FG5eXmlToPdf.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
