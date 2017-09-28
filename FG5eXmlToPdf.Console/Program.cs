023d0f54f41d6922813dc3ed2af45ec8bf6e573b
J.D. Cain <jd.cain.jr@gmail.com>
2017-09-28 12:26:22 -0400

    Added generic  try catch for everything for now.

--- FG5eXmlToPdf.Console/Program.cs
+++ FG5eXmlToPdf.Console/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FG5eXmlToPDF;
+using static System.Console;
 
 namespace FG5eXmlToPdf.Console
 {
@@ -10,6 +13,38 @@ namespace FG5eXmlToPdf.Console
     {
         private static void Main(string[] args)
         {
+            if (args == null | args?.Length == 0)
+            {
+                WriteLine("Requires arg with path to fantasy grounds character xml");
+            }
+            else
+            {
+                if (File.Exists(args[0]))
+                {
+                    try
+                    {
+                        var currentDirectory = System.IO.Directory.GetCurrentDirectory();
+                        var character = FG5eXml.LoadCharacter(args[0]);
+                        var charName = character.Properities.FirstOrDefault((x) => x.Name == "Name")?.Value;
+                        var level = character.Properities.FirstOrDefault((x) => x.Name == "LevelTotal")?.Value;
+                        var outFile = $@"{currentDirectory}\{charName} ({level}).pdf";
+                        FG5ePdf.Write(character,outFile);
+                        WriteLine($"Wrote: {outFile}");
+                    }
+                    catch (Exception e)
+                    {
+                        WriteLine(e);
+                        throw;
+                    }
+
+                }
+                else
+                {
+                    WriteLine("Can't find the file");
+                }
+            }
+
+            
         }
     }
 }
