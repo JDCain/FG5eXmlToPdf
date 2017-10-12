8e1c08d3194d8c1b6742373e6c0b33ef0205abc5
J.D. Cain <jd.cain.jr@gmail.com>
2017-10-12 11:33:40 -0400

    Moved test xml files to their own folder, created unit tests for each file to easily see if one begins to fail.

--- FG5eXmlToPdf.Tests/UnitTest1.cs
+++ FG5eXmlToPdf.Tests/UnitTest1.cs
@@ -10,32 +10,75 @@ namespace FG5eXmlToPdf.Tests
     [TestClass]
     public class UnitTest1
     {
-        private object _charElement;
+        private readonly string _sourceDirectory = $@"{Directory.GetCurrentDirectory()}\XML";
+        //[TestMethod]
+        public void ReadWriteAllTest()
+        {               
+            foreach (var file in Directory.EnumerateFiles(_sourceDirectory, "*.xml"))
+            {
+                GenerateSheet(file, _sourceDirectory);
+            }
+        }
 
-        [TestMethod]
-        public void ReadWriteTest()
+        private static void GenerateSheet(string file, string currentDirectory)
         {
             try
             {
-                var currentDirectory = System.IO.Directory.GetCurrentDirectory();
-                foreach (var file in Directory.EnumerateFiles(currentDirectory, "*.xml"))
-                {
-                    var xml = XDocument.Load(file);
-                    if (xml?.Root?.Element("character") == null) continue;
-                    var character = FG5eXml.LoadCharacter(file);
-                    var charName = character.Properities.FirstOrDefault((x) => x.Name == "Name")?.Value;
-                    var level = character.Properities.FirstOrDefault((x) => x.Name == "LevelTotal")?.Value;
-                    FG5ePdf.Write(
-                        character,
-                        $@"{currentDirectory}\{charName} ({level}).pdf");
-                }
+                var xml = XDocument.Load(Path.Combine(currentDirectory, file));
+                if (xml?.Root?.Element("character") == null) return;
+                var character = FG5eXml.LoadCharacter(Path.Combine(currentDirectory, file));
+                var charName = character.Properities.FirstOrDefault((x) => x.Name == "Name")?.Value;
+                var level = character.Properities.FirstOrDefault((x) => x.Name == "LevelTotal")?.Value;
+                FG5ePdf.Write(
+                    character,
+                    $@"{currentDirectory}\{charName} ({level}).pdf");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
             }
+        }
 
+        [TestMethod]
+        public void RitaTest()
+        {
+            GenerateSheet("rita.xml", _sourceDirectory);
+        }
+        [TestMethod]
+        public void ChuffyTest()
+        {
+            GenerateSheet("chuffy.xml", _sourceDirectory);
+        }
+        [TestMethod]
+        public void PoogTest()
+        {
+            GenerateSheet("poog.xml", _sourceDirectory);
+        }
+        [TestMethod]
+        public void MogmurchTest()
+        {
+            GenerateSheet("mogmurch.xml", _sourceDirectory);
+        }
+        [TestMethod]
+        public void SarahTest()
+        {
+            GenerateSheet("sarah.xml", _sourceDirectory);
+        }
+        [TestMethod]
+        public void TannerTest()
+        {
+            GenerateSheet("tanner.xml", _sourceDirectory);
+        }
+        [TestMethod]
+        public void RavinaTest()
+        {
+            GenerateSheet("ravina.xml", _sourceDirectory);
+        }
+        [TestMethod]
+        public void Faffner20170928Test()
+        {
+            GenerateSheet("faffner20170928.xml", _sourceDirectory);
         }
     }
 }
