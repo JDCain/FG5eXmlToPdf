60643291829b3721fb7d757dd32161b11a4e9f92
J.D. Cain <jd.cain.jr@gmail.com>
2017-10-13 11:09:31 -0400

    removed unused directives.

--- FG5eXmlToPdf/Fg5eXml.cs
+++ FG5eXmlToPdf/Fg5eXml.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Xml.Linq;
-using System.Xml.Serialization;
 using System.Xml.XPath;
 using FG5eXmlToPdf;
 using FG5eXmlToPdf.Models;
